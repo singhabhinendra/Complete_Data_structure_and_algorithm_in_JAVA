2b4c58191f3ad2c8570e048d88cd6d3fe41f7907
singhabhinendra <singhabhinendra337@gmail.com>
2024-10-17 10:36:22 +0530

    first commit

--- leetcode_Questions/FirstLastElement.java
+++ leetcode_Questions/FirstLastElement.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Code_34_firstlastElement {
+public class FirstLastElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
