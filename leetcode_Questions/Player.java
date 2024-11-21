5bb768e5c3c2c704c8f36c5483cbe1247436b36b
singhabhinendra <singhabhinendra337@gmail.com>
2024-11-21 15:12:27 +0530

    Add class

---
+++ leetcode_Questions/Player.java
@@ -0,0 +1,53 @@
+
+//  Array in Real World #1 -Tic-Tac-Toe
+
+public class Player {
+    public static void main(String[] args){
+
+    }
+//  Represent the Game State
+    char[] [] ticTacToe=new char[3][4];
+
+//    Represent Soduku Board
+    int[][] board=new int[9][9];
+//    Create a List of Players
+    Player[] players=new Player[3];
+
+}
+/**
+// Array in Real Word #2 -Shopping App Components
+ class PaymentCategory{
+ int categoryId;
+ int CategoryName;
+ String categoryImgUrl;
+
+ }
+
+ PaymentCategory[] billPayments=new PaymentsCategory[14];
+
+// Update it
+ billPayments[0] =new PaymentCategory (1,"Electricity","someUrl");
+
+
+
+
+// Array in Real World #3 Data Members of a Class
+ class Enemy {
+ Bullet bullets[];
+ Enemy (){
+ bullets=new Bullet[6];
+ }
+ fireBullet(){
+ mkm;
+ }
+ }
+
+
+// Array in Real World #4 : Images
+
+ int [][] image=new int [R][C][3];
+// Here image is a 3D Array
+// NHT image --1920*1080 pixels
+
+
+ */
