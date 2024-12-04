cb5fa3df8afe7336c9e8caf543d4d639f45fbb3e
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-04 22:15:58 +0530

    add CheckIfOneStringIaARotationOfAnother.java

---
+++ geeks-for-geeks-problems/src/CheckIfOneStringIsARotationOfAnother.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CheckIfOneStringIsARotationOfAnother {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        String s1=scanner.nextLine();
+        String s2=scanner.nextLine();
+        if(isRotation(s1,s2)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+    public static boolean isRotation(String s1, String s2){
+        return s1.length() == s2.length() && ( s1 + s1).contains(s2);
+    }
+}
