7ef1c02b218533c4d64136fb32b38c55dee85515
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-04 22:15:04 +0530

    add MinCharsToAddForPalindrome.java

---
+++ geeks-for-geeks-problems/src/MinCharsToAddForPalindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MinCharsToAddForPalindrome {
+    public int minAddToMakeValid(String s){
+        for( int i=0,j=s.length()-1;i<s.length()/2;i++,j--){
+            if(s.charAt(i)!=s.charAt(j)){
+                int end = s.length() - i ;
+                for (int p = 0, q = end; p < s.length() - 1; p++, q--) {
+                    if (s.charAt(p) != s.charAt(q)) {
+                        end--;
+                    }else {
+                        return s.length() - end;
+                    }
+                }
+            }
+            else {
+                return 0;
+            }
+
+        }
+        return 0;
+    }
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+        String str=scanner.nextLine();
+        MinCharsToAddForPalindrome obj=new MinCharsToAddForPalindrome();
+        System.out.println(obj.minAddToMakeValid(str));
+    }
+}
