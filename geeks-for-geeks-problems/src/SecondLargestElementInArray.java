1db54757e536dd49955d7316386e9f3e9b3a1ed5
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-13 18:30:30 +0530

    add SecondLargestElementInArray.java

---
+++ geeks-for-geeks-problems/src/SecondLargestElementInArray.java
@@ -0,0 +1,21 @@
+//Time Complexity: O(n*logn), as sorting the array takes O(n*logn) time and
+// traversing the array
+// can take O(n) time in the worst case,
+// so total time complexity = (n*logn + n) = O(n*logn).
+//Auxiliary space: O(1), as no extra space is required.
+import java.util.Arrays;
+public class SecondLargestElementInArray {
+    static int getSecondLargest(int[] arr){
+        int n=arr.length;
+        Arrays.sort(arr);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]!=arr[n-1]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println(getSecondLargest(new int[]{12,35,1,10,34,35,35}));
+    }
+}
