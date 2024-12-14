824c221981975c1fd89d7daa2745d7ba815a3dd6
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-14 11:18:49 +0530

    add IndexOfAnExtraElement2.java

---
+++ geeks-for-geeks-problems/src/IndexOfAnExtraElement2.java
@@ -0,0 +1,22 @@
+/*Complexity Analysis:
+    Time complexity : O(log n).
+    The time complexity of binary search is O(log n)
+    Space complexity : O(1).
+    As no extra space is required, so the time complexity is constant.
+*/
+public class IndexOfAnExtraElement2 {
+    static int findExtra(int[] arr1,int[] arr2){
+        for(int i=0;i<arr2.length;i++){
+            if(arr1[i]!=arr2[i]) {
+                return i;
+            }
+        }
+        return arr2.length;
+    }
+    public static void main(String[] args){
+        int[] arr1={2,4,6,8,10,12,13};
+        int[] arr2={2,4,6,8,10,12};
+        int n=arr2.length;
+        System.out.println(findExtra(arr1,arr2));
+    }
+}
