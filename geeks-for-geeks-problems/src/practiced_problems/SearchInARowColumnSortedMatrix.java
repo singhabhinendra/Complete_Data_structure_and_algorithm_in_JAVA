1d0a7b6cff39278b0dc8186a1e7a5f7fe2f21cd3
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-22 23:21:23 +0530

    add SearchInARowColumnSortedMatrix.java into practiced_problems package

---
+++ geeks-for-geeks-problems/src/practiced_problems/SearchInARowColumnSortedMatrix.java
@@ -0,0 +1,20 @@
+package practiced_problems;
+
+public class SearchInARowColumnSortedMatrix {
+    public static boolean matSearch(int mat[][], int x) {
+        int rows = mat.length;
+        int columns = mat[0].length;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                if(mat[i][j]==x){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        SearchInARowColumnSortedMatrix solution = new SearchInARowColumnSortedMatrix();
+        System.out.println(solution.matSearch(new int[][]{{1,2},{3,4},{5,6}},2));
+    }
+}
