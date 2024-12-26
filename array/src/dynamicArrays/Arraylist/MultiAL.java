47edfb9cc7ffe05e9024479410355f9926385c47
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-26 17:34:14 +0530

    add MultiAL.java into  ArrayList package

---
+++ array/src/dynamicArrays/Arraylist/MultiAL.java
@@ -0,0 +1,21 @@
+package dynamicArrays.Arraylist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class MultiAL {
+    public static void main(String[] args){
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+        for(int i=0;i<3;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                list.get(i).add(scanner.nextInt());
+            }
+        }
+        System.out.println(list);
+
+    }
+
+}
