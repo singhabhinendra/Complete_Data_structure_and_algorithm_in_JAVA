ad2cb59319ff56e6e3191f2e49562768d7c782e9
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-28 19:31:13 +0530

    add PlusOne.java into programmingskills package

---
+++ leetcode_Questions/programmingskills/PlusOne.java
@@ -0,0 +1,25 @@
+package programmingskills;
+import java.util.Arrays;
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+    public static void main(String[] args) {
+        PlusOne plusOne = new PlusOne();
+        System.out.println(Arrays.toString(plusOne.plusOne(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0})));
+    }
+}
+
+
+
