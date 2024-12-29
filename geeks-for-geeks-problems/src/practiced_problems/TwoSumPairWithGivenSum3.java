7ab5e9c898dcf92ac875cd2d237a06670a583b23
singhabhinendra <singhabhinendra337@gmail.com>
2024-12-29 14:18:46 +0530

    update this code

--- geeks-for-geeks-problems/src/practiced_problems/TwoSumPairWithGivenSum3.java
+++ geeks-for-geeks-problems/src/practiced_problems/TwoSumPairWithGivenSum3.java
@@ -1,7 +1,9 @@
 /*
 * Time Complexity: O(n*log(n)), for sorting the array
 Auxiliary Space: O(1)
-*     The idea is to use the two-pointer technique but for using the two-pointer technique, the array must be sorted. Once the array is sorted then we can use this approach by keeping one pointer at the beginning (left) and another at the end (right) of the array. Then check the sum of the elements at these two pointers:
+* The idea is to use the two-pointer technique but for using the two-pointer technique,
+*  the array must be sorted. Once the array is sorted then we can use this approach by keeping
+* one pointer at the beginning (left) and another at the end (right) of the array. Then check the sum of the elements at these two pointers:
 
         If the sum equals the target, we’ve found the pair.
         If the sum is less than the target, move the left pointer to the right to increase the sum.
