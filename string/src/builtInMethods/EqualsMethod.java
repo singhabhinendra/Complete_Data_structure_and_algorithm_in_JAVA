24cbfc6152d4acb6b8452ff98618225286964e23
singhabhinendra <singhabhinendra337@gmail.com>
2025-01-02 18:51:56 +0530

    add

---
+++ string/src/builtInMethods/EqualsMethod.java
@@ -0,0 +1,9 @@
+package builtInMethods;
+
+public class EqualsMethod {
+    public static void main(String[] args) {
+        String s1="geeksforgeeks",
+                s2="geeksforgeeks";
+        System.out.println(s2.equals(s1));
+    }
+}
