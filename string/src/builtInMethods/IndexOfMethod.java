3bf75951d1bb3d9c18e220e2f9727cabb26305c2
singhabhinendra <singhabhinendra337@gmail.com>
2025-01-02 18:51:39 +0530

    add

---
+++ string/src/builtInMethods/IndexOfMethod.java
@@ -0,0 +1,10 @@
+package builtInMethods;
+
+public class IndexOfMethod {
+    public static void main(String[] args) {
+        String s1="geeksforgeeks";
+        String s2="geeks";
+        System.out.println(s1.indexOf(s2));
+        System.out.println(s1.indexOf(s2,1));
+    }
+}
