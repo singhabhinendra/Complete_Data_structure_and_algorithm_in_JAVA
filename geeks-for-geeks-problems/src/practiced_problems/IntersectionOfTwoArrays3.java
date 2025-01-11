e5b4c6ab71c087ef3848b2a5705f9ce36d4c1826
singhabhinendra <singhabhinendra337@gmail.com>
2025-01-11 17:33:12 +0530

    add IntersectionOfTwoArrays3.java

---
+++ geeks-for-geeks-problems/src/practiced_problems/IntersectionOfTwoArrays3.java
@@ -0,0 +1,11 @@
+package practiced_problems;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//public class IntersectionOfTwoArrays3 {
+//    static List<Integer> intersection(List<Integer> a,List<Integer> b){
+//        Set<Integer> intersection=new HashSet<>();
+//
+//    }
+//}
