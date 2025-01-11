e5eb9c8067187e4f3793e374745a3e4d35137446
singhabhinendra <singhabhinendra337@gmail.com>
2025-01-11 17:29:23 +0530

    add ContainsMethod.java

---
+++ string/src/builtInMethods/ContainsMethod.java
@@ -0,0 +1,10 @@
+package builtInMethods;
+
+public class ContainsMethod {
+    public static void main(String[] args) {
+        String s1="geeksforgeeks";
+        String s2="geeks";
+        System.out.println(s1.contains(s2));
+
+    }
+}
