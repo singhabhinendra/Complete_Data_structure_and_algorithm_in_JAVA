ff308fe9badeaf9c7197b6b460374c2c6166af1b
singhabhinendra <singhabhinendra337@gmail.com>
2025-01-11 17:31:29 +0530

    add DataSizeRange.java

---
+++ basics-description/src/datatypes/DataSizeRange.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class DataSizeRange {
+    public static void main(String[] args){
+        int x;
+        Integer y;
+        System.out.println("Int Min:" + Integer.MIN_VALUE);
+        System.out.println("Int Max:" + Integer.MAX_VALUE);
+        System.out.println("Int Bytes:" + Integer.BYTES);
+
+    }
+}
