d6b3f601e704c22ea2e7ea50a7e6c0fade0412f9
singhabhinendra <singhabhinendra337@gmail.com>
2025-01-14 03:14:58 +0530

    add ChecIfAParanthesStringCanBeValid.java into programmingskills

---
+++ leetcode_Questions/programmingskills/ChecIfAParanthesStringCanBeValid.java
@@ -0,0 +1,42 @@
+/*Example 1: locked String->"010100" s="))())": Output True:
+* Example 2: locked String->"0000" s="()() :Output: true:"
+//* Example 3: locked String-> s="/)" s="0":Output:false
+ */
+package programmingskills;
+public class ChecIfAParanthesStringCanBeValid {
+    public boolean canBeValid(String s,String locked){
+        if(s.length()%2!=0){
+            return false;
+        }
+        int unlockedCount=0,balance=0;
+        for(int i=0;i<s.length();i++){
+            if(locked.charAt(i)=='0'){
+                unlockedCount++;
+            }
+            else if(s.charAt(i)=='1'){
+                balance++;
+            }
+            else{
+                balance--;
+            }
+            if(balance+ unlockedCount<0) return false;
+        }
+        unlockedCount=0;balance=0;
+        for(int i=s.length()-1;i>1;i--){
+            if(locked.charAt(i)=='0'){
+                unlockedCount++;
+            }
+            else if(s.charAt(i)==')'){
+                balance++;
+            }
+            else {
+                balance--;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        ChecIfAParanthesStringCanBeValid obj = new ChecIfAParanthesStringCanBeValid();
+        System.out.println(obj.canBeValid("()()", "0000"));
+    }
+}
