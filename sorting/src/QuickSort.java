7b1f2ca70b0bef98510cd3e59c2704f3849351e8
singhabhinendra <singhabhinendra337@gmail.com>
2025-09-21 09:25:53 +0530

    add QuickSort.java and RecursiveInsertionSort.class

--- sorting/src/QuickSort.java
+++ sorting/src/QuickSort.java
@@ -1,17 +1,47 @@
-public class QuickSort {
-    public static void main(String[] args) {
-        int[] arr={1,2,3,4};
-        System.out.println(partition(arr));
-
-    }
-    public static int partition(int [] arr){
-        int pivot =arr[arr.length-1];
-        int idx=0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]<pivot){
-                idx++;
+import java.util.*;
+public class QuickSort{
+    static int partition(List<Integer> arr,int low,int high){
+        int pivot=arr.get(low);
+        int i=low;
+        int j=high;
+        while(i<j){
+            while(arr.get(i)<=pivot && i<=high-1){
+                i++;
+            }
+            while(arr.get(j)>pivot && j>=low+1){
+                j--;
+            }
+            if(i<j){
+                int temp=arr.get(i);
+                arr.set(i,arr.get(j));
+                arr.set(j,temp);
             }
         }
-        return idx;
+        int temp=arr.get(low);
+        arr.set(low,arr.get(j));
+        arr.set(j,temp);
+        return j;
+    }
+    static void qs(List<Integer> arr, int low,int high){
+        if(low<high){
+            int pIndex=partition(arr,low,high);
+            qs(arr,low,pIndex-1);
+            qs(arr,pIndex+1,high);
+        }
+    }
+    public static List<Integer> quickSort(List<Integer> arr){
+        qs(arr,0,arr.size()-1);
+        return arr;
+    }
+    public static void main(String[] args){
+        List<Integer> arr=new ArrayList<>();
+        arr=Arrays.asList(new Integer[] {4,6,2,5,7,9,1,3});
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            System.out.print(arr.get(i)+" ");
+        }
+        arr=quickSort(arr);
+        System.out.println(arr);
+
     }
 }
